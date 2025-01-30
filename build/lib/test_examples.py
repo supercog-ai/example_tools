149dc2e46888fd63bb1b115c3bfa8c54404424d1
Kenneth Reitz <me@kennethreitz.org>
2025-01-30 14:50:54 -0500

    Add basic test script for LinkedIn and Google News tools

---
+++ build/lib/test_examples.py
@@ -0,0 +1,23 @@
+import asyncio
+from examples.google_news import GoogleNewsTool
+from examples.linkedin_tool import LinkedinDataTool
+
+
+
+async def test_linkedin():
+    linkedin = LinkedinDataTool()
+    res = await linkedin.linkedin_people_search(
+        "Scott Persinger",
+        #company = "Supercog",
+    )
+    print(res)
+
+
+def main():
+    result = asyncio.run(test_linkedin())
+    print(result)
+
+    print(GoogleNewsTool().get_top_headlines())
+
+if __name__ == "__main__":
+    main()

5ff348714d41718498e73fa7abac58665674ea55
Kenneth Reitz <me@kennethreitz.org>
2025-01-31 08:44:33 -0500

    Add news reporter agent for generating news reports from Google News with pydanticai

---
+++ src/agents/pydanticai_agent/news_reporter.py
@@ -0,0 +1,41 @@
+# From https://ai.pydantic.dev/multi-agent-applications/#agent-delegation
+
+from pydantic_ai import Agent, RunContext
+from pydantic_ai.usage import UsageLimits
+
+from tools import GoogleNewsTool
+
+
+producer = Agent(
+    "gpt-4o-mini",
+    system_prompt=(
+        "You are a news producer. Call the reporter with the indicated topic."
+    ),
+)
+news_reporter = Agent(
+    "gpt-4o",
+    system_prompt=(
+        "You are a hard news reporter."
+        "Call Google News to get headlines on the indicated news topic."
+        "Then, respond with an NPR-style news report based on the headlines given."
+    ),
+    result_type=list[str],
+)
+
+
+@producer.tool
+async def call_news_reporter(ctx: RunContext[None], topic: str) -> list[str]:
+    r = await news_reporter.run(topic)
+    return r.data
+
+
+@news_reporter.tool
+async def get_google_news_headlines(ctx: RunContext[None], topic: str) -> list[str]:
+    gnt = GoogleNewsTool()
+    return str(gnt.query_news(topic))
+
+
+result = producer.run_sync(
+    "Get the news about World Finance",
+)
+print(result.data)

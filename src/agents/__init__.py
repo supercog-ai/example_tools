078059a70fe11deffe2a290e52a444fe6f355f2a
Kenneth Reitz <me@kennethreitz.org>
2025-01-31 09:08:50 -0500

    Add pydanticai_agent and simplemind_tools to agent exports

--- src/agents/__init__.py
+++ src/agents/__init__.py
@@ -1,3 +1,3 @@
-from . import swarm_agent
+from . import swarm_agent, pydanticai_agent, simplemind_tools
 
-__all__ = ['swarm_agent']
+__all__ = ["swarm_agent", "pydanticai_agent", "simplemind_tools"]
